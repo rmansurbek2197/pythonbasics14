859dee57d619b286d17eb800a29df2b6da8ea6a5
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-18 23:29:18 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-def ajratib_oling(matn):
-    kichik_harflar = [harf for harf in matn if harf.islower()]
-    return kichik_harflar
+def hisobla(lugat):
+    yigindisi = 0
+    for kalit, qiymat in lugat.items():
+        if isinstance(qiymat, (int, float)):
+            yigindisi += qiymat
+    return yigindisi
 
-matn = "Salom Dunyo"
-print(ajratib_oling(matn))
+lugat = {
+    'a': 10,
+    'b': 20,
+    'c': 30,
+    'd': 'qiqi',
+    'e': 40,
+}
+
+print(hisobla(lugat))
+yigindisi = hisobla(lugat)
+if yigindisi > 50:
+    print("yig'indisi 50 dan katta")
+else:
+    print("yig'indisi 50 dan kichik yoki teng")
+    
+lugat2 = {
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 4,
+    'e': 5,
+}
+
+print(hisobla(lugat2))
+lugat3 = {
+    'a': 100,
+    'b': 200,
+    'c': 'salom',
+    'd': 300,
+}
+print(hisobla(lugat3))
