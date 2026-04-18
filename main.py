c86d97595dcac79c999551112b38ea81c54988ae
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-18 23:35:37 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,38 +1,78 @@
-def hisobla(lugat):
-    yigindisi = 0
-    for kalit, qiymat in lugat.items():
-        if isinstance(qiymat, (int, float)):
-            yigindisi += qiymat
-    return yigindisi
+class Song:
+    def __init__(self, title, artist, duration):
+        self.title = title
+        self.artist = artist
+        self.duration = duration
 
-lugat = {
-    'a': 10,
-    'b': 20,
-    'c': 30,
-    'd': 'qiqi',
-    'e': 40,
-}
+class Playlist:
+    def __init__(self, name):
+        self.name = name
+        self.songs = []
 
-print(hisobla(lugat))
-yigindisi = hisobla(lugat)
-if yigindisi > 50:
-    print("yig'indisi 50 dan katta")
-else:
-    print("yig'indisi 50 dan kichik yoki teng")
-    
-lugat2 = {
-    'a': 1,
-    'b': 2,
-    'c': 3,
-    'd': 4,
-    'e': 5,
-}
+    def add_song(self, song):
+        self.songs.append(song)
 
-print(hisobla(lugat2))
-lugat3 = {
-    'a': 100,
-    'b': 200,
-    'c': 'salom',
-    'd': 300,
-}
-print(hisobla(lugat3))
+    def remove_song(self, song):
+        self.songs.remove(song)
+
+    def display_songs(self):
+        for song in self.songs:
+            print(f"{song.title} by {song.artist}, duration: {song.duration} seconds")
+
+class User:
+    def __init__(self, username):
+        self.username = username
+        self.playlists = []
+
+    def create_playlist(self, playlist):
+        self.playlists.append(playlist)
+
+    def display_playlists(self):
+        for playlist in self.playlists:
+            print(f"Playlist: {playlist.name}")
+            playlist.display_songs()
+            print()
+
+class MusicPlayer:
+    def __init__(self):
+        self.songs = []
+        self.playlists = []
+        self.users = []
+
+    def add_song(self, song):
+        self.songs.append(song)
+
+    def add_playlist(self, playlist):
+        self.playlists.append(playlist)
+
+    def add_user(self, user):
+        self.users.append(user)
+
+    def display_songs(self):
+        for song in self.songs:
+            print(f"{song.title} by {song.artist}, duration: {song.duration} seconds")
+
+    def display_playlists(self):
+        for playlist in self.playlists:
+            print(f"Playlist: {playlist.name}")
+
+    def display_users(self):
+        for user in self.users:
+            print(f"User: {user.username}")
+
+song1 = Song("Song 1", "Artist 1", 180)
+song2 = Song("Song 2", "Artist 2", 240)
+playlist1 = Playlist("Playlist 1")
+playlist1.add_song(song1)
+playlist1.add_song(song2)
+user1 = User("User 1")
+user1.create_playlist(playlist1)
+music_player = MusicPlayer()
+music_player.add_song(song1)
+music_player.add_song(song2)
+music_player.add_playlist(playlist1)
+music_player.add_user(user1)
+music_player.display_songs()
+music_player.display_playlists()
+music_player.display_users()
+user1.display_playlists()
